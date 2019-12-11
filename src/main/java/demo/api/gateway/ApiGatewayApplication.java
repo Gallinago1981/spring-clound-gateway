8dbc476992d805a3543ea4f3c64e8d47db0cf213
snipes81 <snipes81b-github@yahoo.co.jp>
2019-12-11 15:01:46 +0900

    Initial commit

---
+++ src/main/java/demo/api/gateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package demo.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
